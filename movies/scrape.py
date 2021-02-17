e8653c78d9af96c0ef5130e81a3900ba709990b6
Animesh Sharma <animesharma3@gmail.com>
2021-02-17 08:50:43 +0530

    Update scrape.py

--- movies/scrape.py
+++ movies/scrape.py
@@ -1,9 +1,10 @@
 import pandas as pd
 import requests
+import ast
 
 def get_top_movies(df, n):
     result  = []
-    for i in range(len(df[:n])):
+    for i in range(len(df[:n+1])):
         title = df.iloc[i]['title']
         imdb_id = df.iloc[i]['imdb_id']
         url = 'https://www.omdbapi.com/?apikey=69de4384&i={}'.format(imdb_id) # Import data from API
@@ -14,6 +15,29 @@ def get_top_movies(df, n):
         poster_img = data['Poster']
         result.append([title, genre, poster_img, imdb_id]) # Combine both the data
     return result
+
+def filter_movies_by_genre(name, df, N):
+    count = 0
+    result = []
+    for i in range(len(df)):
+        genres = df.iloc[i].genres
+        genres = ast.literal_eval(genres)
+        for genre in genres:
+            if genre['name'] == name:
+                title = df.iloc[i]['title']
+                imdb_id = df.iloc[i]['imdb_id']
+                url = 'https://www.omdbapi.com/?apikey=69de4384&i={}'.format(imdb_id) # Import data from API
+                response = requests.get(url)
+                data = response.json()
+                # print(data.keys())
+                genre = data['Genre']
+                poster_img = data['Poster']
+                result.append([title, genre, poster_img, imdb_id]) 
+                count += 1
+                # print(title, result)
+                continue
+            if count == N:
+                return result
 
 def get_movie_data(df, imdb):
     url = 'https://www.omdbapi.com/?apikey=69de4384&i={}'.format(imdb) 
